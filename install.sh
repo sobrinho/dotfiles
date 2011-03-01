1d8cc2c8a6588ea9a64e103639bb713bbb2141d3
Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
2011-03-01 10:06:43 -0300

    global gitignore

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 ln -si $PWD/ackrc ~/.ackrc
 ln -si $PWD/rvmrc ~/.rvmrc
+ln -si $PWD/gitignore ~/.gitignore
 ln -si $PWD/bash_profile ~/.bash_profile
+
+git config --global core.excludesfile ~/.gitignore
